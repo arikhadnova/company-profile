612bdba15c490fdfe4071ebeee182088cd7b24e5
Ari Khadnova <triuliamrikhadnova2000@gmail.com>
2026-02-03 09:43:28 +0700

    chore: Menghapus skrip utilitas dan berkas data terkait seeding, migrasi, dan pemeriksaan basis data.

--- check_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'app/config.php';
-require_once 'app/core/Database.php';
-
-$db = new Database();
-$db->query("DESCRIBE impact_data");
-$result = $db->resultSet();
-
-echo json_encode($result, JSON_PRETTY_PRINT);

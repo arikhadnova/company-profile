ba141a4abc57d652173ba24a8c2aaf5098c4a872
Ari Khadnova <triuliamrikhadnova2000@gmail.com>
2026-02-05 19:21:18 +0700

    feat: Hapus skrip dump data video dan layanan

--- dump_services.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'app/config.php';
-require 'app/core/Database.php';
-$db = new Database();
-$db->query('SELECT title_id, title_en FROM gi_services');
-$results = $db->resultSet();
-echo json_encode($results, JSON_PRETTY_PRINT);
--- dump_videos.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'app/config.php';
-require 'app/core/Database.php';
-$db = new Database();
-$db->query('SELECT * FROM gi_videos');
-$results = $db->resultSet();
-echo json_encode($results, JSON_PRETTY_PRINT);
